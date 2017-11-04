527b452c6b676e32f368b3b757dd1ee96f3383de
Sunil <sunhick@gmail.com>
2017-11-04 13:26:59 -0700

    added gn file

---
+++ gn/src/main.cc
@@ -0,0 +1,13 @@
+/*
+ * main.cc - Demonstrates the usage of gn to generate ninja files
+ * 	     for compiling the code.
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  cout << "Using chromium/depot_tools/gn!" << "\n";
+  return 0;
+}
